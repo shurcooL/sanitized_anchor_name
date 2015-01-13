9a8b7d4e8f347bfa230879db9d7d4e4d9e19f962
Dmitri Shuralyov <shurcooL@gmail.com>
2015-01-12 19:39:41 -0800

    Fix test to import moved package.

--- main_test.go
+++ main_test.go
@@ -3,7 +3,7 @@ package sanitized_anchor_name_test
 import (
 	"fmt"
 
-	"github.com/shurcooL/go/github_flavored_markdown/sanitized_anchor_name"
+	"github.com/shurcooL/sanitized_anchor_name"
 )
 
 func ExampleCreate() {
